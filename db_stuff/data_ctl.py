064a811a6b0c6f691b0acfd92a077aa96fb126c4
Arseniy <arsenator337@gmail.com>
2024-06-10 21:34:44 +0300

    Changes:     - add languges list

--- db_stuff/data_ctl.py
+++ db_stuff/data_ctl.py
@@ -3,23 +3,27 @@ import pathlib
 import io
 import os
 
-import fitz
+from pdf2image import convert_from_path
 import pytesseract
 from PIL import Image
 
 def read_dict(file_path: str) -> str:
-    print(f"reading data from {file_path}")
-
-    document = fitz.open(file_path)
+    print(f"reading data from {file_path}", flush=True)
+    langs = ""
+    with open(os.path.join(
+                pathlib.Path(__file__).parent.resolve(),
+                "data", "languages.txt")) as f:
+        langs = f.read()
+    langs = "+".join(langs.splitlines()[1:])
+    langs = "+".join(["rus", "eng", "pol", "Greek", "fra", "pol",
+                      "urk", "uzb", "tur", "uzb_cyrl"]) # TODO: delete this line
+    print(f"using languages from languages.txt:\n{langs}", flush=True)
     
-    text = ""
-    for page_num in range(len(document)):
-        page = document.load_page(page_num)
-        pix = page.get_pixmap()
-        img = Image.open(io.BytesIO(pix.tobytes(output="png")))
-        
-        text += pytesseract.image_to_string(img)
-        print(len(text))
+    images = convert_from_path(file_path)
+
+    for page_num, img in enumerate(images):
+        text = pytesseract.image_to_string(img, lang=langs)
+        print(f"Page {page_num + 1}\n{text}\n", flush=True)
     
 
 def main() -> None:
